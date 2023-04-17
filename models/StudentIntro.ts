f4ef14ba6859781eb79e15f056e2467ba2fd1115
Darío Rumbo <dario-r@moegui.com>
2023-04-17 12:05:14 -0300

    Coded handleTransactionSubmit and StudentIntro model

--- models/StudentIntro.ts
+++ models/StudentIntro.ts
@@ -1,3 +1,5 @@
+import * as borsh from '@project-serum/borsh'
+
 export class StudentIntro {
     name: string;
     message: string;
@@ -12,4 +14,18 @@ export class StudentIntro {
         new StudentIntro('Jack Nicholson', `I want to overhaul the world's financial system. Lower friction payments/transfer, lower fees, faster payouts, better collateralization for loans, etc.`),
         new StudentIntro('Terminator', `i'm basically here to protect`),
     ]
+
+    borshInstructionSchema = borsh.struct([
+		borsh.u8('variant'), // unsigned, 8-bit integer representing which function on the program should be called
+		borsh.str('name'),
+		borsh.str('message'),
+	])
+
+    serialize(): Buffer {
+		const buffer = Buffer.alloc(1000)
+		this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer)
+        // We remove the extra space (getSpan is like length)
+		return buffer.slice(0, this.borshInstructionSchema.getSpan(buffer))
+	}
+
 }
